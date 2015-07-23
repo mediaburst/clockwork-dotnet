e97777d9f3aff3efbba1e1f4d86a6f9d0ea2496b
Martin Steel <martin@mediaburst.co.uk>
2015-07-23 10:06:59 +0100

    Nuspec file for publishing

--- Clockwork PCL/Properties/AssemblyInfo.cs
+++ Clockwork PCL/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Clockwork PCL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Clockwork")]
+[assembly: AssemblyDescription("SMS API Wrapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Clockwork PCL")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Mediaburst Ltd")]
+[assembly: AssemblyProduct("Clockwork")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
