f5afef080408fc6f4d298dc2abb8ff34b2bef2dc
Karl Green <karltgreen@gmail.com>
2017-10-17 12:02:57 +0100

    Added the Clockwork namespace to the AccountType andInvalidCharacterAction enums to prevent clashing with enums of the same name in a project that uses Clockwork

--- Clockwork/AccountType.cs
+++ Clockwork/AccountType.cs
@@ -1,11 +1,14 @@
-﻿public enum AccountType
+﻿namespace Clockwork
 {
-    /// <summary>
-    /// Pay As You Go - Purchase credit online
-    /// </summary>
-    PayAsYouGo,
-    /// <summary>
-    /// Invoice account - Billed at the end of the month
-    /// </summary>
-    Invoice
+	public enum AccountType
+	{
+		/// <summary>
+		/// Pay As You Go - Purchase credit online
+		/// </summary>
+		PayAsYouGo,
+		/// <summary>
+		/// Invoice account - Billed at the end of the month
+		/// </summary>
+		Invoice
+	}
 }
--- Clockwork/InvalidCharacterAction.cs
+++ Clockwork/InvalidCharacterAction.cs
@@ -1,25 +1,27 @@
-﻿
-/// <summary>
-/// Action to take if the message text contains an invalid character
-/// Valid characters are defined in the GSM 03.38 character set
-/// </summary>
-public enum InvalidCharacterAction
+﻿namespace Clockwork
 {
-    /// <summary>
-    /// Use the default setting from your account
-    /// </summary>
-    AccountDefault = 0,
-    /// <summary>
-    /// Return an error if a Non-GSM character is found
-    /// </summary>
-    Error = 1,
-    /// <summary>
-    /// Remove any Non-GSM character
-    /// </summary>
-    Remove = 2,
-    /// <summary>
-    /// Replace Non-GSM characters where possible
-    /// remove any others
-    /// </summary>
-    Replace = 3
+	/// <summary>
+	/// Action to take if the message text contains an invalid character
+	/// Valid characters are defined in the GSM 03.38 character set
+	/// </summary>
+	public enum InvalidCharacterAction
+	{
+		/// <summary>
+		/// Use the default setting from your account
+		/// </summary>
+		AccountDefault = 0,
+		/// <summary>
+		/// Return an error if a Non-GSM character is found
+		/// </summary>
+		Error = 1,
+		/// <summary>
+		/// Remove any Non-GSM character
+		/// </summary>
+		Remove = 2,
+		/// <summary>
+		/// Replace Non-GSM characters where possible
+		/// remove any others
+		/// </summary>
+		Replace = 3
+	}
 }
